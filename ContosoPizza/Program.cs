80f60eb2f4142ab5a93c6ee2d9f3c1bd2a81a622
Cam Soper <Cam.Soper@microsoft.com>
2023-05-02 17:29:13 -0500

    Set default state

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -10,8 +10,6 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<PizzaContext>(options =>
     options.UseSqlite("Data Source=ContosoPizza.db"));
 
-builder.Services.AddScoped<PizzaService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
