588f898c12f2bb6fb790b99bc2707ccbaa96fa1c
Cam Soper <cam.soper@microsoft.com>
2023-05-12 11:34:56 -0500

    Starter project ready for revisions (#1)
    * initial commit
    
    * tweaks
    
    * Finished state
    
    * finished state
    
    * Set default state
    
    * dev container and more

---
+++ ContosoPizza/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoPizza.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+    public PizzaSize Size { get; set; }
+    public bool IsGlutenFree { get; set; }
+
+    [Range(0.01, 9999.99)]
+    public decimal Price { get; set; }
+}
+
+public enum PizzaSize { Small, Medium, Large }
---
+++ ContosoPizza/Services/PizzaService.cs
@@ -0,0 +1,46 @@
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services
+{
+    public class PizzaService
+    {
+        private readonly PizzaContext _context = default!;
+
+        public PizzaService(PizzaContext context) 
+        {
+            _context = context;
+        }
+        
+        public IList<Pizza> GetPizzas()
+        {
+            if(_context.Pizzas != null)
+            {
+                return _context.Pizzas.ToList();
+            }
+            return new List<Pizza>();
+        }
+
+        public void AddPizza(Pizza pizza)
+        {
+            if (_context.Pizzas != null)
+            {
+                _context.Pizzas.Add(pizza);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeletePizza(int id)
+        {
+            if (_context.Pizzas != null)
+            {
+                var pizza = _context.Pizzas.Find(id);
+                if (pizza != null)
+                {
+                    _context.Pizzas.Remove(pizza);
+                    _context.SaveChanges();
+                }
+            }            
+        } 
+    }
+}
