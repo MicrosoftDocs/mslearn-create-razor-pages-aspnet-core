1d9d1a545a3acea2f7fdc66c36cbd2ee504f389d
styazhkoV <alexsandr.styazhkov@gmail.com>
2023-10-01 23:54:26 +0600

    add PageList.razor and PageList.chtml and front

---
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ControsoPizza.Pages
+{
+    public class PizzaListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
