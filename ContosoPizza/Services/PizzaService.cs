797a1795d69545f800e64b7aa9283b843be1aafd
Alex Wolf <alexwolf@microsoft.com>
2023-10-23 14:36:11 -0400

    fix namespaces

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -1,46 +1,45 @@
 ﻿using ContosoPizza.Data;
 using ContosoPizza.Models;
 
-namespace ContosoPizza.Services
+namespace ContosoPizza.Services;
+
+public class PizzaService
 {
-    public class PizzaService
+    private readonly PizzaContext _context = default!;
+
+    public PizzaService(PizzaContext context) 
     {
-        private readonly PizzaContext _context = default!;
-
-        public PizzaService(PizzaContext context) 
+        _context = context;
+    }
+    
+    public IList<Pizza> GetPizzas()
+    {
+        if(_context.Pizzas != null)
         {
-            _context = context;
+            return _context.Pizzas.ToList();
         }
-        
-        public IList<Pizza> GetPizzas()
-        {
-            if(_context.Pizzas != null)
-            {
-                return _context.Pizzas.ToList();
-            }
-            return new List<Pizza>();
-        }
+        return new List<Pizza>();
+    }
 
-        public void AddPizza(Pizza pizza)
+    public void AddPizza(Pizza pizza)
+    {
+        if (_context.Pizzas != null)
         {
-            if (_context.Pizzas != null)
+            _context.Pizzas.Add(pizza);
+            _context.SaveChanges();
+        }
+    }
+
+    public void DeletePizza(int id)
+    {
+        if (_context.Pizzas != null)
+        {
+            var pizza = _context.Pizzas.Find(id);
+            if (pizza != null)
             {
-                _context.Pizzas.Add(pizza);
+                _context.Pizzas.Remove(pizza);
                 _context.SaveChanges();
             }
-        }
-
-        public void DeletePizza(int id)
-        {
-            if (_context.Pizzas != null)
-            {
-                var pizza = _context.Pizzas.Find(id);
-                if (pizza != null)
-                {
-                    _context.Pizzas.Remove(pizza);
-                    _context.SaveChanges();
-                }
-            }            
-        } 
-    }
+        }            
+    } 
 }
